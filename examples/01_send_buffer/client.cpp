d7269fd8f54bbb8e34db1903f97976c4fc79bedd
Alberto Miranda <alberto.miranda@bsc.es>
2018-11-30 14:54:15 +0100

    Add send_buffer example

---
+++ examples/01_send_buffer/client.cpp
@@ -0,0 +1,161 @@
+#include <vector>
+#include <iostream>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include <hermes.hpp>
+
+#include "rpcs.hpp"
+
+std::tuple<hermes::transport, std::string>
+parse_args(int argc, char* argv[]) {
+
+    if(argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " ADDRESS\n";
+
+        std::cerr << 
+            "Address formats:\n"                                           <<
+            "  [BMI plugin]\n"                                             <<
+            "    tcp:      bmi+tcp[://<hostname,IP>:<port>]\n\n"           <<
+            "  [MPI plugin]\n"                                             <<
+            "    static:   mpi+static\n"                                   <<
+            "    dynamic:  mpi+dynamic\n\n"                                <<
+            "  [SM plugin]\n"                                              <<
+            "    sm:       na+sm\n\n"                                      <<
+            "  [OFI/libfabric plugin]\n"                                   <<
+            "    tcp:      ofi+tcp[://<hostname,IP,interface>:<port>]\n"   <<
+            "    verbs:    ofi+verbs[://<hostname,IP,interface>:<port>]\n" <<
+            "    psm2:     ofi+psm2\n"                                     <<
+            "    gni:      ofi+gni[://<hostname,IP,interface>]\n\n"        <<
+            "  [CCI (deprecated)]\n"                                       <<
+            "    tcp:      cci+tcp[://<hostname,IP,interface>:<port>]\n"   <<
+            "    verbs:    cci+verbs[://<hostname,IP,interface>:<port>]\n" <<
+            "    sm:       cci+sm[://<PID>/<ID>]\n";
+
+        exit(1);
+    }
+
+    const std::string address(argv[1]);
+
+    std::size_t pos = address.find("://");
+
+    if(pos == std::string::npos) {
+        std::cout << "WARNING: Address does not include a transport prefix. "
+                     "Defaulting to ofi+tcp\n";
+
+        return std::make_tuple(hermes::transport::ofi_tcp, address);
+    }
+    else {
+        return std::make_tuple(
+                hermes::get_transport_type(address.substr(0, pos)),
+                address.substr(pos+3));
+    }
+}
+
+int
+main(int argc, char* argv[]) {
+
+    using hermes::transport;
+    using hermes::async_engine;
+    using hermes::endpoint_set;
+    using hermes::output_set;
+    //using hermes::rpc;
+    using hermes::access_mode;
+    //using hermes::send_buffer_args;
+
+    hermes::transport tr;
+    std::string target_address;
+
+    std::tie(tr, target_address) = parse_args(argc, argv);
+
+    try {
+
+        // initialize the engine with appropriate transport protocol
+        async_engine hg(transport::ofi_tcp);
+
+        hermes::endpoint endp = hg.lookup(target_address);
+
+        // start the asynchronous engine
+        hg.run();
+
+        // define and expose any buffers that we want transferred 
+        // so that they can be accessed remotely through RMA
+        char data[]  = {"These are the contents of an example buffer"};
+        auto mapped_file = 
+            std::make_shared<hermes::mapped_buffer>(
+                    "examples/01_send_buffer/lipsum.txt");
+
+        std::vector<hermes::mutable_buffer> bufvec {
+            hermes::mutable_buffer{data, sizeof(data)},
+            hermes::mutable_buffer{mapped_file->data(), mapped_file->size()},
+        };
+
+        hermes::exposed_memory exposed_buffers = 
+            hg.expose(bufvec, access_mode::read_only);
+
+
+        std::cout << "Sending RPC (send_buffer)\n";
+
+        // Option 1: 
+        //   Instantiate an [RPC]::input object containing the arguments and
+        //   pass it to the post() function
+        example_rpcs::send_buffer::input in3a("test3a", exposed_buffers);
+        auto rpc2a = hg.post<example_rpcs::send_buffer>(endp, in3a);
+
+
+        std::cout << "Sending RPC (send_buffer)\n";
+
+        // Option 2: 
+        //   Instantiate an [RPC]::input object containing the arguments 
+        //   directly as a temporary in the post() function
+        auto rpc2b = 
+            hg.post<example_rpcs::send_buffer>(
+                    endp, example_rpcs::send_buffer::input(
+                            "test3b", exposed_buffers));
+
+
+        std::cout << "Sending RPC (send_buffer)\n";
+
+        // Option 3: 
+        //   Pass the RPC arguments directly to the post() function
+        auto rpc2c = hg.post<example_rpcs::send_buffer>(
+                        endp, "test3c", exposed_buffers);
+
+
+        std::cout << "Waiting for RPC outputs...\n";
+
+        // wait for results
+        hermes::output_set<example_rpcs::send_buffer> results2a = rpc2a.get();
+        hermes::output_set<example_rpcs::send_buffer> results2b = rpc2b.get();
+        hermes::output_set<example_rpcs::send_buffer> results2c = rpc2c.get();
+
+        std::cout << "Output received (size: " << results2a.size() << ")\n";
+        std::cout << "Output received (size: " << results2b.size() << ")\n";
+        std::cout << "Output received (size: " << results2c.size() << ")\n";
+
+        for(auto&& rv : results2a) {
+            std::cout << "retval: " << rv.retval() << "\n";
+        }
+
+        for(auto&& rv : results2b) {
+            std::cout << "retval: " << rv.retval() << "\n";
+        }
+
+        for(auto&& rv : results2c) {
+            std::cout << "retval: " << rv.retval() << "\n";
+        }
+
+        std::cout << "Sending [shutdown]\n";
+
+        hg.post<example_rpcs::shutdown>(endp);
+    } 
+    catch(const std::exception& ex) {
+        std::cerr << ex.what() << "\n";
+    }
+
+    return 0;
+}
---
+++ examples/01_send_buffer/server.cpp
@@ -0,0 +1,174 @@
+#include <unistd.h>
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include <hermes.hpp>
+
+#include "rpcs.hpp"
+
+std::atomic<bool> shutdown_requested(false);
+
+void
+shutdown_handler(hermes::request<example_rpcs::shutdown>&& req) {
+
+    std::cout << "RPC received:\n";
+    std::cout << "    type: shutdown\n";
+    std::cout << "  requires_response? " 
+              << std::boolalpha << req.requires_response() << "\n"; 
+
+    bool expected = false;
+    while(!shutdown_requested.compare_exchange_weak(expected, true) 
+            && !expected);
+}
+
+std::tuple<hermes::transport, std::string>
+parse_args(int argc, char* argv[]) {
+
+    if(argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " ADDRESS\n";
+
+        std::cerr << 
+            "Address formats:\n"                                           <<
+            "  [BMI plugin]\n"                                             <<
+            "    tcp:      bmi+tcp[://<hostname,IP>:<port>]\n\n"           <<
+            "  [MPI plugin]\n"                                             <<
+            "    static:   mpi+static\n"                                   <<
+            "    dynamic:  mpi+dynamic\n\n"                                <<
+            "  [SM plugin]\n"                                              <<
+            "    sm:       na+sm\n\n"                                      <<
+            "  [OFI/libfabric plugin]\n"                                   <<
+            "    tcp:      ofi+tcp[://<hostname,IP,interface>:<port>]\n"   <<
+            "    verbs:    ofi+verbs[://<hostname,IP,interface>:<port>]\n" <<
+            "    psm2:     ofi+psm2\n"                                     <<
+            "    gni:      ofi+gni[://<hostname,IP,interface>]\n\n"        <<
+            "  [CCI (deprecated)]\n"                                       <<
+            "    tcp:      cci+tcp[://<hostname,IP,interface>:<port>]\n"   <<
+            "    verbs:    cci+verbs[://<hostname,IP,interface>:<port>]\n" <<
+            "    sm:       cci+sm[://<PID>/<ID>]\n";
+
+        exit(1);
+    }
+
+    const std::string address(argv[1]);
+
+    std::size_t pos = address.find("://");
+
+    if(pos == std::string::npos) {
+        std::cout << "WARNING: Address does not include a transport prefix. "
+                     "Defaulting to ofi+tcp\n";
+
+        return std::make_tuple(hermes::transport::ofi_tcp, address);
+    }
+    else {
+        return std::make_tuple(
+                hermes::get_transport_type(address.substr(0, pos)),
+                address.substr(pos+3));
+    }
+}
+
+int
+main(int argc, char* argv[]) {
+
+    try {
+
+        hermes::transport tr;
+        std::string bind_address;
+
+        std::tie(tr, bind_address) = parse_args(argc, argv);
+
+        // initialize the engine with the provided address
+        hermes::async_engine hg(tr, bind_address, true);
+
+        // define and register handlers for any defined rpcs
+        const auto send_buffer_handler = 
+            [&](hermes::request<example_rpcs::send_buffer>&& req) {
+
+                example_rpcs::send_buffer::input args = req.args();
+
+                hermes::exposed_memory remote_buffers = args.buffers();
+
+                std::cout << "RPC received:\n";
+                std::cout << "    type: send_buffer,\n"; 
+                std::cout << "    args: remote_buffers{count="
+                          << remote_buffers.count() 
+                          << ", total_size=" << remote_buffers.size() << " }\n"; 
+
+                // let's prepare some local buffers
+                std::vector<hermes::mutable_buffer> bufseq;
+                bufseq.reserve(remote_buffers.count());
+
+                for(auto&& rbuf : remote_buffers) {
+                    std::size_t remote_size = rbuf.size();
+                    char* data = new char[remote_size];
+                    bufseq.emplace_back(data, remote_size);
+                }
+
+                hermes::exposed_memory local_buffers =
+                    hg.expose(bufseq, hermes::access_mode::write_only);
+
+                std::cout << "  Pulling remote buffers\n";
+
+                // this lambda will be invoked when the pull transfer completes
+                // (we capture bufvec by reference to verify that remote data
+                // was actually copied into the buffers, but it's not necessary
+                // in real code)
+                const auto do_pull_completion = [bufseq, &hg](
+                        hermes::request<example_rpcs::send_buffer>&& req) {
+
+                    std::cout << "    Pull successful!\n";
+
+                    for(auto&& buf : bufseq) {
+                        std::cout << "     Buffer size: " << buf.size() << "\n";
+                        std::cout << "     Initial buffer contents:\n" 
+                                  << ">>>> BUFFER START <<<<\n"
+                                  << std::string(
+                                          reinterpret_cast<char*>(
+                                              buf.data()), 
+                                          std::min(buf.size(), 250ul))
+                                  << "\n>>>> BUFFER END <<<<\n";
+                    }
+
+                    if(req.requires_response()) {
+
+                        std::cout << "  Sending response...\n";
+
+                        example_rpcs::send_buffer::output rv(42);
+                        hg.respond<example_rpcs::send_buffer>(
+                                std::move(req), rv);
+
+                        std::cout << "  Response sent with value " 
+                                  << rv.retval() << "\n";
+                    }
+                };
+
+                hg.async_pull(remote_buffers,
+                              local_buffers,
+                              std::move(req),
+                              do_pull_completion);
+            };
+
+        hg.register_handler<example_rpcs::send_buffer>(send_buffer_handler);
+
+        hg.register_handler<example_rpcs::shutdown>(shutdown_handler);
+
+        std::cout << "Listening for requests\n";
+
+        // start the engine
+        hg.run();
+
+        while(!shutdown_requested) {
+            // the server could do actual useful work here while the
+            // engine processes rpcs and invokes handlers
+            sleep(1);
+        }
+
+        std::cout << "Shutting down\n";
+    } 
+    catch(const std::exception& ex) {
+        std::cerr << ex.what() << "\n";
+    }
+
+    return 0;
+}
