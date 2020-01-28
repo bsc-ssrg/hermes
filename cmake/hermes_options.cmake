a6ca160fe400ea50b8b566e80e31aed09d9317d9
Alberto Miranda <alberto.miranda@bsc.es>
2020-01-28 13:10:46 +0100

    Improve CMake messages

--- cmake/hermes_options.cmake
+++ cmake/hermes_options.cmake
@@ -5,8 +5,8 @@ option(HERMES_BUILD_PEDANTIC "Enable extra warnings and expensive tests." OFF)
 option(HERMES_BUILD_WERROR "Halt the compilation with an error on compiler warnings." OFF)
 option(HERMES_BUILD_EXAMPLES "Build the examples." OFF)
 option(HERMES_LOGGING "Enable logging messages (using the fmt library)" OFF)
-cmake_dependent_option(HERMES_LOGGING_USE_BUNDLED_FMT "Use the fmt library bundled with hermes instead of the one installed on the system" ON "HERMES_LOGGING" OFF)
-cmake_dependent_option(HERMES_LOGGING_FMT_HEADER_ONLY "Use the fmt library as header-only" OFF "HERMES_LOGGING" OFF)
+cmake_dependent_option(HERMES_LOGGING_FMT_HEADER_ONLY "Use fmt library as header-only" OFF "HERMES_LOGGING" OFF)
+cmake_dependent_option(HERMES_LOGGING_FMT_USE_BUNDLED "Use the fmt library bundled with hermes instead of the one installed on the system" ON "HERMES_LOGGING" OFF)
 
 # Options that control generation of various targets.
 option(HERMES_DOC "Generate the doc target." ${MASTER_PROJECT})
